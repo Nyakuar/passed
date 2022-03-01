16f6e9dc7e1f3cfca009de4e1701dd0651b8ac5a
Nyakuar <nyakuaryy@gmail.com>
2022-03-01 23:03:08 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,52 @@
+fun main(){
+    school()
+    nameandAge()
+    length()
+    myname()
+
+
+}
+fun school(){
+    var name="Akirachix"
+    println(name[1].toString()+name[3]+name[4])
+}
+
+fun nameandAge(){
+    var name= "Hi my name is Nyakuar i am"
+    var age= "22 years old"
+    var statement= name+" "+ age
+    println(statement )
+}
+
+fun length(){
+    var Job ="Software engineer"
+    println(Job.length)
+}
+
+
+fun myname(){
+    var name = "Nyakuar"
+    if (name=="Gatwech") {
+        println("that is me ")
+    }else
+
+        println("I don't know who that is")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
